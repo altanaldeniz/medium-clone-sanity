cf9a7ca13bb0d2dd61a2fc559357e79523a1efe6
altan <altanaldeniz@gmail.com>
2022-02-01 04:47:32 +0300

    fixed query

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -205,7 +205,7 @@ export const getStaticPaths = async () => {
 };
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
-  const query = `*[_type == "post" && slug.current == 'another-lorem'][0]{
+  const query = `*[_type == "post" && slug.current == $slug][0]{
     _id,
     _createdAt,
     title,
